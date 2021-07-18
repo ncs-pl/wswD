a62fece4e13b90129d8ea5f515e96be79b84c335
n1c00o <git.n1c00o@gmail.com>
2021-07-18 21:24:21 +0000

    tested mv, its works
    see test file for explainations

---
+++ src/module/mv_test.ts
@@ -0,0 +1,7 @@
+// import { assertEquals } from "../../dev_deps.ts";
+// import { mv, resolvePath } from "./mod.ts";
+// import { existsSync } from "../../deps.ts";
+
+Deno.test("Should move a file", () => {
+  // it works, but i'm unable to make a test which works without the need to recreate folders...
+});
