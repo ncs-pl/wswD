65507fc37c418979af4f1be0cf9b34c0f07d785f
n1c00o <git.n1c00o@gmail.com>
2021-07-18 20:12:08 +0000

    add some forgotten types

--- src/module/cat_test.ts
+++ src/module/cat_test.ts
@@ -2,8 +2,8 @@ import { assertEquals } from "../../dev_deps.ts";
 import { cat, resolvePath } from "./mod.ts";
 
 Deno.test("Should print the content of tests/file.txt", () => {
-  const content = Deno.readTextFileSync(resolvePath(import.meta.url, "../../tests/file.txt"));
-  const fileContent = cat(resolvePath(import.meta.url, "../../tests/file.txt"));
+  const content: string = Deno.readTextFileSync(resolvePath(import.meta.url, "../../tests/file.txt"));
+  const fileContent: string = cat(resolvePath(import.meta.url, "../../tests/file.txt"));
 
   assertEquals(content, fileContent[0]);
 });
