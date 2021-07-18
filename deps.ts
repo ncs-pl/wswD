f3bade47bfa7b06d02ffdbbea296089b19c1239d
n1c00o <git.n1c00o@gmail.com>
2021-07-18 21:11:02 +0000

    add mkdir test

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export { homedir } from "https://x.nest.land/std@0.101.0/node/os.ts";
-export { copySync, moveSync } from "https://x.nest.land/std@0.101.0/fs/mod.ts";
+export { copySync, moveSync, existsSync } from "https://x.nest.land/std@0.101.0/fs/mod.ts";
 export { readLines } from "https://x.nest.land/std@0.101.0/io/mod.ts";
 export { delay } from "https://x.nest.land/std@0.101.0/async/mod.ts";
 export {
