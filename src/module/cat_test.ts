614ab350733107490d3a93549c6bde9da0d4b78a
n1c00o <git.n1c00o@gmail.com>
2021-07-18 20:12:17 +0000

    add some forgotten types

--- src/module/cat_test.ts
+++ src/module/cat_test.ts
@@ -3,7 +3,7 @@ import { cat, resolvePath } from "./mod.ts";
 
 Deno.test("Should print the content of tests/file.txt", () => {
   const content: string = Deno.readTextFileSync(resolvePath(import.meta.url, "../../tests/file.txt"));
-  const fileContent: string = cat(resolvePath(import.meta.url, "../../tests/file.txt"));
+  const fileContent: string[] = cat(resolvePath(import.meta.url, "../../tests/file.txt"));
 
   assertEquals(content, fileContent[0]);
 });
