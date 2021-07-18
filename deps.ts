3568d9d14a9656996d963fbfc916d562154cf9f6
n1c00o <git.n1c00o@gmail.com>
2021-07-18 19:47:48 +0000

    deps: add new deps and dev_deps

--- deps.ts
+++ deps.ts
@@ -2,3 +2,11 @@ export { homedir } from "https://x.nest.land/std@0.101.0/node/os.ts";
 export { copySync, moveSync } from "https://x.nest.land/std@0.101.0/fs/mod.ts";
 export { readLines } from "https://x.nest.land/std@0.101.0/io/mod.ts";
 export { delay } from "https://x.nest.land/std@0.101.0/async/mod.ts";
+export {
+  dirname,
+  fromFileUrl,
+  isAbsolute,
+  join,
+  resolve,
+  toFileUrl,
+} from "https://x.nest.land/std@0.101.0/path/mod.ts";
---
+++ dev_deps.ts
@@ -0,0 +1,1 @@
+export { assertEquals } from "https://x.nest.land/std@0.101.0/testing/asserts.ts";
